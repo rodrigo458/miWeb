a99a891c4d23aea9aa803d837800ade6704fc4d3
rodrigo <rodrigogalarza458@gmail.com>
2020-10-06 03:07:01 -0500

    Se ha creado un prpyecto en .NET

---
+++ ProyectosNET/LP2Soft/LP2Soft/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP2Soft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Mi primer programa");
+            System.Console.Read();
+
+        }
+    }
+}
